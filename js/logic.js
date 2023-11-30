23125bbaee884ee4162c4f83c003d908615aafb4
Def-ulT <vadgunbin@yandex.ru>
2023-11-30 21:50:40 +0300

    Add check on fool

--- js/logic.js
+++ js/logic.js
@@ -1,11 +1,13 @@
 
 const input_file = document.querySelector('.file input');
 const button_file = document.querySelector('.file button');
-
+let output = document.querySelector('.output p');
 input_file.style.opacity = 0;
 
 
 button_file.addEventListener('click', () => {
+
+    clearOutput()
 
     input_file.click()
 
@@ -31,26 +33,57 @@ calculate.addEventListener('click', () => {
     const endingRadio = document.querySelectorAll('.tuple_block p input[name="2"]');
     endingSystem = getRadioValue(endingRadio);
 
+    // if (startingSystem || endingSystem == 'undefined') {
+    //     alert("Пожалуйста, выберите системы счисления чисел")
+    // }
+
     const input_num = document.querySelector('.input input').value;
     res = convert(input_num, startingSystem, endingSystem);
 
-    const output = document.querySelector('.output p');
     output.textContent = res.toUpperCase();
+
+    if (output.textContent == 'NAN') {
+        switch (startingSystem) {
+            case '2':
+                alert("Двоичное число принимает на вход только цифры 0 и 1")
+                clearOutput()
+                break
+            case '8':
+                alert("Восьмеричное число принимает на вход только цифры (0-7)")
+                clearOutput()
+                break
+            case '10':
+                alert("Десятичное число принимает на вход только цифры (0-9)")
+                clearOutput()
+                break
+            case '16':
+                alert("Шестнадцатиричное число принимает на вход только цифры (0-9), а также буквы из латинского алфавита (A-F)")
+                clearOutput()
+                break
+            default:
+                alert("Пожалуйста, введите число")
+                clearOutput()
+                break
+        }
+    }
+
+    function convert(input_num, startingSystem, endingSystem) {
+        input_num = parseInt(input_num, startingSystem).toString(endingSystem);
+        return input_num;
+    }
+
+    function getRadioValue(radioName) {
+        for (const radio of radioName) {
+            if (radio.checked) {
+                return radio.value;
+            }
+        }
+    }
+
+    
 
 })
 
-function convert(input_num, startingSystem, endingSystem) {
-    input_num = parseInt(input_num, startingSystem).toString(endingSystem);
-    return input_num;
-}
-
-function getRadioValue(radioName) {
-    for (const radio of radioName) {
-        if (radio.checked) {
-            return radio.value;
-        }
-    }
-}
 
 const saveButton = document.querySelector('#saveButton')
 
@@ -63,6 +96,15 @@ saveButton.addEventListener('click', () => {
     el.click();
     document.body.removeChild(el);
 })
+
+function clearOutput() {
+    output.textContent = ''
+}
+
+
+
+
+
 
 
 
