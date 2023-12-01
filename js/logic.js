be318b648ebbf9615b9149212e0761f08c2af29d
Def-ulT <vadgunbin@yandex.ru>
2023-12-01 18:46:47 +0300

    Переделал обработку ошибок

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,7 @@
 const input_file = document.querySelector('.file input');
 const button_file = document.querySelector('.file button');
 input_file.style.opacity = 0;
+let falseInputData = false
 
 document.querySelector('.input input').value = ''
 
@@ -11,7 +12,16 @@ const defaultEndingRadio = document.querySelector('.tuple_block p input[name="2"
 defaultStartingRadio.click()
 defaultEndingRadio.click()
 
+function clearOutput() {
+    output.textContent = ''
+}
+
 button_file.addEventListener('click', () => {
+    if (output.textContent == 'NAN') {
+        alert("Пожалуйста, введите число")
+        clearOutput()
+        falseInputData = true
+        } 
 
     clearOutput()
 
@@ -33,6 +43,9 @@ button_file.addEventListener('click', () => {
 
 const calculate = document.querySelector('#calculate');
 calculate.addEventListener('click', () => {
+
+    clearOutput()
+ 
     
     const startingRadio = document.querySelectorAll('.tuple_block p input[name="1"]');
     startingSystem = getRadioValue(startingRadio);
@@ -40,39 +53,60 @@ calculate.addEventListener('click', () => {
     endingSystem = getRadioValue(endingRadio);
 
     const input_num = document.querySelector('.input input').value;
-    res = convert(input_num, startingSystem, endingSystem);
 
-    output.textContent = res.toUpperCase();
+    function checkInputData(startingSystem) {
 
-    if (output.textContent == 'NAN') {
-        if (document.querySelector('.input input').value == '') {
-            alert("Пожалуйста, введите число")
-            clearOutput()
-        }   else {
-                switch (startingSystem) {
-                    case '2':
+        DictionaryFor2 = ['0', '1']
+        DictionaryFor8 = ['0', '1', '2', '3', '4', '5', '6', '7']
+        DictionaryFor10 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        DictionaryFor16 = ['0', '1', '2', '3', '4', '5', '6', '7', 'A', '8', '9', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f']
+        const numberOfDictionary = [DictionaryFor2, DictionaryFor8, DictionaryFor10, DictionaryFor16]
+        let convert_startingSystem = {
+            '2': 0,
+            '8': 1,
+            '10': 2,
+            '16': 3,
+        }
+        convert_startingSystem = convert_startingSystem[startingSystem]
+    
+        for (const element of input_num) {
+
+            if (!numberOfDictionary[convert_startingSystem].includes(element)) {
+                switch(convert_startingSystem) {
+                    case 0:
                         alert("Двоичное число принимает на вход только цифры 0 и 1")
-                        clearOutput()
                         break
-                    case '8':
+                    case 1:
                         alert("Восьмеричное число принимает на вход только цифры (0-7)")
-                        clearOutput()
                         break
-                    case '10':
+
+                    case 2:
                         alert("Десятичное число принимает на вход только цифры (0-9)")
-                        clearOutput()
                         break
-                    case '16':
-                        alert("Шестнадцатиричное число принимает на вход только цифры (0-9), а также буквы из латинского алфавита (A-F)")
-                        clearOutput()
-                        break
-                    default:
-                        alert("Пожалуйста, введите число")
-                        clearOutput()
+                    case 3:
+                        alert("Шестнадцатеричное число принимает на вход только цифры (0-9), а также буквы из латинского алфавита (A-F)")
                         break
                 }
+                falseInputData = true
+                clearOutput()
+                return falseInputData
             }
+        };
     }
+    checkInputData(startingSystem)
+
+    if (!(falseInputData)) {
+        res = convert(input_num, startingSystem, endingSystem);
+        output.textContent = res.toUpperCase();
+
+        }
+    
+    if (output.textContent == 'NAN') {
+        alert("Пожалуйста, введите число")
+        clearOutput()
+        falseInputData = true
+        }
+
 
     function convert(input_num, startingSystem, endingSystem) {
         input_num = parseInt(input_num, startingSystem).toString(endingSystem);
@@ -88,7 +122,7 @@ calculate.addEventListener('click', () => {
     }
 
     
-
+    falseInputData = false
 })
 
 
@@ -104,9 +138,9 @@ saveButton.addEventListener('click', () => {
     document.body.removeChild(el);
 })
 
-function clearOutput() {
-    output.textContent = ''
-}
+
+
+
 
 
 
